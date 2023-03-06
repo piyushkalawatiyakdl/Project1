ebb05f97ab0fbc5f3eb576bb903f31a0af1ca934
unknown <you@example.com>
2023-03-06 15:12:25 +0530

    fmfmwrfm

---
+++ dom.js
@@ -0,0 +1,9 @@
+var x= document.getElementsByClassName("list-group-item")
+console.log(x)
+// console.log(x)
+// document.getElementById
+
+console.log(x[0])
+x[1].textContent="Updated item"
+x[1].style.fontWeight='bold';
+x[2].style.backgroundColor="green"
